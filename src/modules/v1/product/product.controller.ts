f3747383eab6cfa509334523f80e4de1e2a63151
Zechariah-dev <omoladesunday2017@gmail.com>
2023-07-05 04:19:36 +0100

    feat: product controller swagger docs

--- src/modules/v1/product/product.controller.ts
+++ src/modules/v1/product/product.controller.ts
@@ -14,7 +14,6 @@ import {
   HttpCode,
   HttpStatus,
   Query,
-  BadRequestException,
   UnprocessableEntityException,
 } from "@nestjs/common";
 import { ProductService } from "./product.service";
@@ -23,7 +22,16 @@ import { UpdateProductDto } from "./dto/update-product.dto";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 import { FilesInterceptor } from "@nestjs/platform-express";
 import { PaginationParams } from "./dto/pagination-params.dto";
-import { ApiQuery, ApiTags } from "@nestjs/swagger";
+import {
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from "@nestjs/swagger";
 
 @Controller("product")
 @UseGuards(JwtAuthGuard)
@@ -37,6 +45,9 @@ export class ProductController {
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @UseInterceptors(FilesInterceptor("files"))
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({ description: "Creeate user product" })
+  @ApiCreatedResponse({ description: "product created successfully" })
   public async create(
     @Body() body: CreateProductDto,
     @UploadedFiles() files: Array<Express.Multer.File>,
@@ -71,6 +82,8 @@ export class ProductController {
     name: "limit",
     type: "number",
   })
+  @ApiOperation({ description: "Fetch user product" })
+  @ApiOkResponse({ description: "product fetched successfully" })
   public async getUserProducts(
     @Query() { page, limit }: PaginationParams,
     @Req() req
@@ -86,6 +99,10 @@ export class ProductController {
 
   @Patch()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ description: "Update user product" })
+  @ApiOkResponse({ description: "product updated successfully" })
+  @ApiUnprocessableEntityResponse({ description: "unable to update product" })
+  @ApiParam({ name: "id", type: "string" })
   public async editProduct(
     @Param("id") id: string,
     @Body() body: UpdateProductDto,
@@ -102,6 +119,10 @@ export class ProductController {
 
   @Delete()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ description: "Delete user product" })
+  @ApiOkResponse({ description: "product deleted successfully" })
+  @ApiUnprocessableEntityResponse({ description: "unable to delete product" })
+  @ApiParam({ name: "id", type: "string" })
   public async deleteProduct(@Param("id") id: string, @Req() req) {
     const product = await this.productService.delete(id, req.user._id);
 
