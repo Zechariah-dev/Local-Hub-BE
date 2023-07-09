1396bbe54f0d61e96d16eb1003c2b75d3cf6d686
Zechariah-dev <omoladesunday2017@gmail.com>
2023-07-09 14:04:31 +0100

    fix: update user login auth dto

--- src/modules/v1/auth/dto/login-auth.dto.ts
+++ src/modules/v1/auth/dto/login-auth.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginAuthDto {
   @IsString()
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 }
