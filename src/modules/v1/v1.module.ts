8b257fee1203aa6d7a7f1ef5ab9f81d6f5b394d0
Zechariah-dev <omoladesunday2017@gmail.com>
2023-07-19 17:53:20 +0100

    feat: setup order module

--- src/modules/v1/v1.module.ts
+++ src/modules/v1/v1.module.ts
@@ -4,6 +4,7 @@ import { ServiceModule } from "./service/service.module";
 import { CloudinaryModule } from "./cloudinary/cloudinary.module";
 import { UserModule } from "./user/user.module";
 import { CategoryModule } from "./category/category.module";
+import { OrdersModule } from "./orders/orders.module";
 
 @Module({
   providers: [
@@ -12,6 +13,7 @@ import { CategoryModule } from "./category/category.module";
     ServiceModule,
     CloudinaryModule,
     UserModule,
+    OrdersModule,
   ],
 })
 export class V1Module {}
